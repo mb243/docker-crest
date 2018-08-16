325fe1283aad68cf978a3449091f8453ec979b6f
Mike Beach <9483554+mb243@users.noreply.github.com>
2018-08-15 22:28:01 -0400

    Nothing to see here >_>

--- distros/debian.9.sh
+++ distros/debian.9.sh
@@ -93,7 +93,7 @@ install_docker() {
     software-properties-common
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   add-apt-repository \
-     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+     "deb [arch=amd64] https://download.docker.com/linux/debian \
      $(lsb_release -cs) \
      stable"
   apt-get update
